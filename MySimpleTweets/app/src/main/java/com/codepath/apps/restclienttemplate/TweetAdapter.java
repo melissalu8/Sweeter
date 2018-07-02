520c6d002bd83dbe62f37c80fb423a1429bff22b
Melissa Lu <melissalu@fb.com>
2018-07-02 15:46:11 -0700

    Complete Timeline

---
+++ MySimpleTweets/app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -0,0 +1,84 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> {
+
+    private List<Tweet> mTweets;
+    // pass in the Tweets array in the constructor
+    public TweetAdapter(List<Tweet> tweets) {
+        mTweets = tweets;
+    }
+
+    // for each row, inflate the layout and cache (pass) references into ViewHolder
+
+    @NonNull
+    @Override
+    // this method is only invoked when you need to create a new row
+    // onCreateViewHolder inflates the layout and caches all the findViewById lookups
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // inflate the layout you created
+        // need to first get context
+        Context context = parent.getContext();
+        // get the layout using the context object
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // inflate item_tweet
+        View tweetView = inflater.inflate(R.layout.item_tweet, parent, false);
+        ViewHolder viewHolder = new ViewHolder(tweetView);
+        return viewHolder;
+    }
+
+
+    // Or else will call on onBindViewHolder
+    // bind the values based on the position of the element
+    // passes a position and the previously cached ViewHolder and repopulate the data based on the position of that element
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // get the data according to position
+        Tweet tweet = mTweets.get(position);
+
+        // populate the views according to this data
+        holder.tvUsername.setText(tweet.user.name);
+        holder.tvBody.setText(tweet.body);
+
+        // TODO: profile image using Glide
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTweets.size();
+    }
+
+    // create ViewHolder class
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView ivProfileImage;
+        public TextView tvUsername;
+        public TextView tvBody;
+
+        public ViewHolder (View itemView) {
+            super(itemView);
+
+            // perform findViewById lookups
+
+            ivProfileImage = (ImageView) itemView.findViewById(R.id.ivProfileImage);
+            tvUsername = (TextView) itemView.findViewById(R.id.tvUserName);
+            tvBody = (TextView) itemView.findViewById(R.id.tvBody);
+        }
+    }
+
+
+}
---
+++ MySimpleTweets/app/src/main/java/com/codepath/apps/restclienttemplate/models/SampleModelDao.java
@@ -0,0 +1,26 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface SampleModelDao {
+
+    // @Query annotation requires knowing SQL syntax
+    // See http://www.sqltutorial.org/
+    
+    @Query("SELECT * FROM SampleModel WHERE id = :id")
+    SampleModel byId(long id);
+
+    @Query("SELECT * FROM SampleModel ORDER BY ID DESC LIMIT 300")
+    List<SampleModel> recentItems();
+    // pull the last 300 entries in the table
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertModel(SampleModel... sampleModels);
+    // to insert new entries
+}
